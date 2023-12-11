62ef03b158a7ca7223acb6283a280f4081533cd3
Amiram Yass <amiram-yss@hotmail.com>
2023-12-11 18:53:49 +0200

    works, tends to put many points on one spot. min 5 unique points. dbg tools developed.

--- nav_obj_tracker/NavTracker.cpp
+++ nav_obj_tracker/NavTracker.cpp
@@ -39,10 +39,14 @@ void NavTracker::NextFrame(Mat& next_image) {
     }
 
     current_img = gray_next_image.clone();
-    std::cout << "removed " << removed_ctr << " points." << std::endl;
+    
+    //std::cout << "Frame points: " << std::endl;
+    //for (auto p : this->feature_points)
+    //    std::cout << p << std::endl;
+
     for (size_t i = 0; i < removed_ctr; i++)
     {
-        std::cout << "iter " << i << std::endl;
+        //std::cout << "iter " << i << std::endl;
         CreateNewPoint();
     }
 }
@@ -58,59 +62,99 @@ void NavTracker::ShowImgWithFeatures() const
     for (auto p : this->feature_points)
         circle(cpy, p, 8, Scalar(0, 0, 255), 2);
     imshow("PrintMarkedImg()", cpy);
-    std::cout << "PrintMarkedImg() called" << std::endl;
     waitKey();
 }
 
 void NavTracker::CreateNewPoint()
 {
+    std::unique(this->feature_points.begin(), this->feature_points.end());
     // search in lower half and higher half x vals
-    int mid_x = current_img.cols;
-    int mid_y = current_img.rows;
+    int mid_x = current_img.cols / 2;
+    int mid_y = current_img.rows / 2;
 
-    int roi_width = (mid_x * 3) / 4;
-    int roi_height = (mid_y * 3) / 4;
+    int roi_width = (mid_x * 2) / 3;
+    int roi_height = (mid_y * 2) / 3;
 
-    int ctr_pts_up = 0,
+    int ctr_pts_dn = 0,
         ctr_pts_rt = 0;
 
     for (auto p : feature_points) {
-        if (p.x > mid_y)
-            ++ctr_pts_up;
+        if (p.y > mid_y)
+            ++ctr_pts_dn;
         else
-            --ctr_pts_up;
+            --ctr_pts_dn;
         if (p.x > mid_x)
             ++ctr_pts_rt;
         else
             --ctr_pts_rt;
     }
 
-    int roi_x = ctr_pts_rt < 0 ? mid_x - roi_width : mid_x;
-    int roi_y = ctr_pts_rt < 0 ? mid_y - roi_height : mid_y;
+    int roi_x = ctr_pts_rt > 0 ? mid_x - roi_width : mid_x;
+    int roi_y = ctr_pts_dn > 0 ? mid_y - roi_height : mid_y;
     Rect roi_region(roi_x, roi_y, roi_width, roi_height);
     Mat roi = this->current_img(roi_region);
-    equalizeHist(roi, roi);
-
+    //equalizeHist(roi, roi);
+    
     std::vector<Point2f> feature_result;
 
     int max_min = 0;
     Point2f max_min_point;
 
-    goodFeaturesToTrack(roi, feature_result, 10, 0.01, 5. /*maybe more?*/, noArray(), 27, true, 0.04);
+    goodFeaturesToTrack(roi, feature_result, 10, 0.01, 25. /*maybe more?*/, noArray(), 27, true, 0.04);
+
+    Mat animation = this->current_img.clone();
+    cvtColor(animation, animation, COLOR_GRAY2BGR);
+    for (auto p : feature_points)
+        circle(animation, p, 8, Scalar(0, 255, 255), 2);
+
     for (auto p : feature_result) {
         int min = pow(mid_x,2);
-        for (auto p2 : this->feature_points) {
-            if (pow(p.x - p2.x, 2) + pow(p.y - p2.y, 2) < min)
-                min = pow(p.x - p2.x, 2) + pow(p.y - p2.y, 2);
+        for (auto p2 : this -> feature_points) {
+            Point2f p2c(p2.x + roi_x, p2.y + roi_y);
+            int dist_sqrd = pow(p.x - p2c.x, 2) + pow(p.y - p2c.y, 2);
+            //std::cout << "dist: " << p << " to " << p2c << ": " << dist_sqrd << std::endl;
+            if (dist_sqrd < min) {
+                min = pow(p.x - p2c.x, 2) + pow(p.y - p2c.y, 2);
+                std::cout << p << " - calced min: " << min << std::endl;
+            }
         }
-        if (min < max_min)
-            return;
+        circle(animation, p, 8, Scalar(100,50,50), 2);
+        if (min < max_min) {
+            imshow("distance selection animation", animation);
+            waitKey();
+            continue;
+        }
+        circle(animation, p, 8, Scalar(255, 100, 50), 2);
+        std::cout << "cur max min: " << max_min << ", new max min: " << min << std::endl;
         max_min = min;
         max_min_point = p;
+        imshow("distance selection animation", animation);
+        waitKey();
     }
-    if (ctr_pts_rt > 0)
-        max_min_point.x += mid_x;
-    if (ctr_pts_up > 0)
-        max_min_point.x += mid_y;
+
+    std::cout << "min max pint: " << max_min_point << std::endl;
+
+    //if (ctr_pts_rt > 0)
+    max_min_point.x += roi_x;
+    //if (ctr_pts_dn > 0)
+    max_min_point.y += roi_y;
+    std::cout << "vector size: " << this->feature_points.size() << std::endl;
     this->feature_points.push_back(max_min_point);
+    std::cout << this->feature_points.size() << std::endl;
+
+    //dbg
+    Mat cpy;
+    cpy = this->current_img.clone();
+    cvtColor(cpy, cpy, COLOR_GRAY2BGR);
+    rectangle(cpy, roi_region, Scalar(100,100,50), 2);
+    for (auto p : feature_result) {
+        Point2f calP(p.x + roi_x, p.y + roi_y);
+        circle(cpy, calP, 8, Scalar(255, 0, 0), 2);
+    }
+    for (auto p : this->feature_points)
+        circle(cpy, p, 8, Scalar(0, 0, 255), 2);
+    circle(cpy, max_min_point, 8, Scalar(0, 255, 0), 2);
+    imshow("new point view", cpy);
+    waitKey();
+    ///
 }
--- nav_obj_tracker/nav_obj_tracker.cpp
+++ nav_obj_tracker/nav_obj_tracker.cpp
@@ -8,16 +8,17 @@
 int main()
 {
     std::cout << "Hello World!\n";
-    std::string img_add_prefix = "C:\\Users\\amira\\OneDrive\\Desktop\\harris corner detection algorithm\\pics\\frames 1b\\frameNo";
-    int initIndex = 538;
+    std::string img_add_prefix = "C:\\Users\\Amiram Yassif\\Documents\\pics\\frames 1b\\frameNo";
+    int initIndex = 2300; // 538;
     cv::Mat initImg = imread(img_add_prefix + std::to_string(initIndex) + ".jpg");
-    imshow("", initImg);
-    waitKey();
+    circle(initImg, Point(0, 0), 8, Scalar(255, 0, 0), 3);
+    //imshow("", initImg);
+    //waitKey();
     std::cout << "tracker init" << std::endl;
     NavTracker nav(initImg, 20.);
     nav.ShowImgWithFeatures();
     Mat img;
-    for (size_t i = 539; i < 2640; i++)
+    for (size_t i = initIndex; i > 538; i--)
     {
         img = imread(img_add_prefix + std::to_string(i) + ".jpg");
         nav.NextFrame(img);
